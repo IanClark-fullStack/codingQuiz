04daba1637f915f5628d889d53cfd24a97128354
Ian Clark <IanClark2021@u.northwestern.edu>
2021-10-01 14:28:27 -0500

    Starting Pseudocode: By adding markup to html outlining how data will change with functionality

---
+++ Assets/JS/script.js
@@ -0,0 +1,11 @@
+/*_________________ Quiz Main Functionality _________________*/
+    // Array that we can always randomize - using fisher-yates 
+        // Each Element should be equal to it's own Question Object
+            Question-Object {
+                'question': 'prompt', 
+                'answers: ['question1' 'question2', 'question3', 'question4'],
+                
+                
+            }
+
+    // Title Heading - Values change to that of a Question 
