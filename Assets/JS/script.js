4d323f7a13e4d06a5d5a7bded6e7c509741e7d44
Ian Clark <IanClark2021@u.northwestern.edu>
2021-10-03 12:14:05 -0500

    OOP Method 1: Using the Functions Object version to package together data and functionality by implenting the 'new' constructor and 'this' keyword.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,11 +1,34 @@
-/*_________________ Quiz Main Functionality _________________*/
-    // Array that we can always randomize - using fisher-yates 
-        // Each Element should be equal to it's own Question Object
-            Question-Object {
-                'question': 'prompt', 
-                'answers: ['question1' 'question2', 'question3', 'question4'],
-                
-                
-            }
 
-    // Title Heading - Values change to that of a Question 
+// Method to Add Code and Paint GenerateIntroPage on Page
+function GenerateIntroPage(headId, headText, pId, pText) {
+    // Define variables within prototype object
+    this.headId = headId || "";
+    this.headText = headText || "";
+    this.pId = pId || ""; 
+    this.pText = pText || "";
+
+    // Define the container & create elements
+    var headingContainer = document.querySelector('header');
+    var headingText = document.createElement('h1');
+    var mainContainer = document.querySelector('main');
+    var createParagraph = document.createElement('p');
+    // Assign new elements with their resources
+    headingText.id = headId; 
+    headingText.textContent = headText;
+    createParagraph.textContent = this.pText;
+    createParagraph.id = this.pId;
+    headingText.setAttribute('style', 'font-family: courier, monospace; font-weight: 300; text-shadow: 3px 1px #3485e761; font-sizeL 2.9em;');
+
+   
+    // Paint Elements to Page
+    this.paintStartPage = function() {
+        headingContainer.appendChild(headingText);
+        mainContainer.appendChild(createParagraph); // Append h1, 
+    }
+}
+
+// Prepare resources for the new elements
+var startPage = new GenerateIntroPage('headingIntro', 'Code Quiz', 'quizInfo', quizInto); // code loaded 
+
+// Paint New Elements to page 
+startPage.paintStartPage();
